5d096ca32e1b622787d93446f56eaa7993b9a02d
jm_weather <jm@nexmodeling.com>
2016-10-13 12:48:00 +0200

    update the demo

--- demo/daily2.R
+++ demo/daily2.R
@@ -1,5 +1,5 @@
 #############################################################################################################
-# Get the daily temperature amd precipitation at station no 180 between three days in past from now and now #
+# Get the daily temperature amd precipitation at station no 180 of the last three days                      #
 #############################################################################################################
 library(wsklimaR)
 tmp <- wsklimaR.getValues(timeserietypeID="0",stations="180",elements=c("TAM","RR"))
